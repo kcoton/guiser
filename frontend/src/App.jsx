0d60c4e6fb5f000d1d0efbadd1fb0508d5bf027f
shumin11 <shumin11@student.ubc.ca>
2024-06-05 19:04:06 -0700

    fix the route issue and organize folder structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,43 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LandingPage from './components/LandingPage/LandingPage';
-import LoginPage from './components/LoginPage/LoginPage';
-import './App.css'
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import LandingPage from './pages/LandingPage/LandingPage';
+import LoginPage from './pages/LoginPage/LoginPage';
 import NavigationBar from './components/NavigationBar';
+import HomePage from './pages/HomePage';
+import PersonasPage from './pages/PersonasPage';
+import ConnectPage from './pages/ConnectPage';
+import GeneratePage from './pages/GeneratePage/GeneratePage';
+import PostsPage from './pages/PostsPage';
+import SettingsPage from './pages/SettingsPage';
+import LogoutPage from './pages/LogoutPage';
+import './App.css'
 
 function App() {
   return (
-      <Router>
-          <div className="App">
-              <Routes>
-                  <Route path="/" element={<LandingPage />} />
-                  <Route path="/login" element={<LoginPage />} />
-                  <Route path='/dashboard' element={<NavigationBar />} />
-              </Routes>
-          </div>
-      </Router>
+    <Router>
+      <MainApp />
+    </Router>
+  );
+}
+
+function MainApp() {
+  const location = useLocation();
+
+  return (
+    <div className="App">
+      {location.pathname !== '/' && location.pathname !== '/login' && <NavigationBar />}
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path='/dashboard' element={<HomePage />} />
+        <Route path="/personas" element={<PersonasPage />} />
+        <Route path="/connect" element={<ConnectPage />} />
+        <Route path="/generate" element={<GeneratePage />} />
+        <Route path="/posts" element={<PostsPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/logout" element={<LogoutPage />} />
+      </Routes>
+    </div>
   );
 }
 
