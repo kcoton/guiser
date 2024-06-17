bf83df48541b407f2222bafeb60023eee3c95f6a
Brant Shapka <bshapka@gmail.com>
2024-06-17 00:12:05 -0700

    replaced tabs with spaces

--- backend/src/server/Server.ts
+++ backend/src/server/Server.ts
@@ -10,67 +10,67 @@ import * as http from "http";
 import cors from "cors";
 
 export default class Server {
-	private readonly port: number;
-	private express: Application;
-	private server: http.Server | undefined;
+    private readonly port: number;
+    private express: Application;
+    private server: http.Server | undefined;
 
-	constructor(port: number) {
-		this.port = port;
-		this.express = express();
-		this.registerMiddleware();
-		this.registerRoutes();
-	}
+    constructor(port: number) {
+        this.port = port;
+        this.express = express();
+        this.registerMiddleware();
+        this.registerRoutes();
+    }
 
-	public start(): Promise<void> {
-		return new Promise((resolve, reject) => {
-			if (this.server !== undefined) {
-				reject();
-			} else {
-				this.server = this.express.listen(this.port, () => {
-					resolve();
-				}).on("error", (err: Error) => {
-					reject(err);
-				});
-			}
-		});
-	}
+    public start(): Promise<void> {
+        return new Promise((resolve, reject) => {
+            if (this.server !== undefined) {
+                reject();
+            } else {
+                this.server = this.express.listen(this.port, () => {
+                    resolve();
+                }).on("error", (err: Error) => {
+                    reject(err);
+                });
+            }
+        });
+    }
 
-	public stop(): Promise<void> {
-		return new Promise((resolve, reject) => {
-			if (this.server === undefined) {
-				reject();
-			} else {
-				this.server.close(() => {
-					resolve();
-				});
-			}
-		});
-	}
+    public stop(): Promise<void> {
+        return new Promise((resolve, reject) => {
+            if (this.server === undefined) {
+                reject();
+            } else {
+                this.server.close(() => {
+                    resolve();
+                });
+            }
+        });
+    }
 
-	private registerMiddleware() {
-		this.express.use(express.json());
-		this.express.use(cors());
-	}
+    private registerMiddleware() {
+        this.express.use(express.json());
+        this.express.use(cors());
+    }
 
-	private registerRoutes() {
-		this.express.get("/echo/:msg", Server.echo);
-	}
+    private registerRoutes() {
+        this.express.get("/echo/:msg", Server.echo);
+    }
 
-	private static echo(req: Request, res: Response) {
-		try {
-			console.log(`Server::echo(..) - params: ${JSON.stringify(req.params)}`);
-			const response = Server.performEcho(req.params.msg);
-			res.status(200).json({result: response});
-		} catch (err) {
-			res.status(400).json({error: err});
-		}
-	}
+    private static echo(req: Request, res: Response) {
+        try {
+            console.log(`Server::echo(..) - params: ${JSON.stringify(req.params)}`);
+            const response = Server.performEcho(req.params.msg);
+            res.status(200).json({result: response});
+        } catch (err) {
+            res.status(400).json({error: err});
+        }
+    }
 
-	private static performEcho(msg: string): string {
-		if (typeof msg !== "undefined" && msg !== null) {
-			return `${msg}...${msg}`;
-		} else {
-			return "Message not provided";
-		}
-	}
+    private static performEcho(msg: string): string {
+        if (typeof msg !== "undefined" && msg !== null) {
+            return `${msg}...${msg}`;
+        } else {
+            return "Message not provided";
+        }
+    }
 }
