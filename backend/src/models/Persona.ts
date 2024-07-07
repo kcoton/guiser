5c649a3faeae8243fe5e0dbff0a39cdda0f73499
Brant Shapka <bshapka@gmail.com>
2024-07-06 21:43:34 -0700

    added persona model

---
+++ backend/src/models/Persona.ts
@@ -0,0 +1,23 @@
+import mongoose, { Document, Schema } from 'mongoose';
+import { ContentSchema, IContent} from './Content';
+
+export interface IPersona extends Document {
+    name: string;
+    text: string;
+    content: IContent[];
+}
+
+export const PersonaSchema: Schema = new Schema<IPersona>({
+    name: { type: String, required: true },
+    text: { type: String, required: true },
+    content: [ContentSchema]
+}, {
+    timestamps: true
+});
+
+const Persona = mongoose.model<IPersona>(
+    'Persona', 
+    PersonaSchema
+);
+
+export default Persona;
