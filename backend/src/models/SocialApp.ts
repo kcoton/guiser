84770da022b6b313f7754bd5dc8c984b67aed6c8
Brant Shapka <bshapka@gmail.com>
2024-07-06 19:27:26 -0700

    adjusted formatting

--- backend/src/models/SocialApp.ts
+++ backend/src/models/SocialApp.ts
@@ -1,15 +1,19 @@
 import mongoose, { Document, Schema } from 'mongoose';
 
 interface ISocialApp extends Document {
-  seq_no: number;
-  name: string;
+    seqNo: number;
+    name: string;
 }
 
 const SocialAppSchema: Schema = new Schema<ISocialApp>({
-  seq_no: { type: Number, required: true },
-  name: { type: String, required: true }
+    seqNo: { type: Number, required: true },
+    name: { type: String, required: true }
 });
 
-const SocialApp = mongoose.model<ISocialApp>('SocialApp', SocialAppSchema, 'social_app');
+const SocialApp = mongoose.model<ISocialApp>(
+    'SocialApp', 
+    SocialAppSchema, 
+    'social_app'
+);
 
 export default SocialApp;
