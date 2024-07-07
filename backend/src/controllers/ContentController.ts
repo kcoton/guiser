5357616039c0bbe27cdcfc8b084e50b42368ea4f
Brant Shapka <bshapka@gmail.com>
2024-07-06 17:40:58 -0700

    added missing type

--- backend/src/controllers/ContentController.ts
+++ backend/src/controllers/ContentController.ts
@@ -13,7 +13,7 @@ export default class ContentController {
         const { persona, promptContext } = req.body as { persona: IPersona; promptContext: string };
 
         try {
-            const content = await this.genAiService.getTextContent(persona, promptContext);
+            const content: string = await this.genAiService.getTextContent(persona, promptContext);
             res.status(200).json({ result: content });
         } catch (error) {
             res.status(500).json({ error: (error as Error).message });
