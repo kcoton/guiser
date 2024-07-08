ce8f93532476539b098191389916925a556347ec
Brant Shapka <bshapka@gmail.com>
2024-07-07 22:58:04 -0700

    implemented create persona action

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import User, { IUser } from "../models/User";
+import { IPersona } from "../models/Persona";
+import mongoose from "mongoose";
 
 export default class UserController {
     public getUser = async (req: Request, res: Response): Promise<void> => {
@@ -12,13 +14,13 @@ export default class UserController {
 
             const user: IUser | null = await User.findOne({ externalId: externalId }).lean();
             if (!user) {
-                res.status(404).json({ error: `User with matching externalId not found.` });
+                res.status(404).json({ error: `user with matching externalId not found` });
                 return;
             }
 
             res.status(200).json({ result: user });
         } catch (error) {
-            this.handleError(res, error);
+            this.handleGeneralError(res, error);
         }
     }
 
@@ -34,15 +36,47 @@ export default class UserController {
             res.status(200).json({ result: user });
         } catch (error) {
             if ((error as any)?.code === 11000) {
-                res.status(409).json({ error: 'User with externalId already exists.' });
+                res.status(409).json({ error: 'user with externalId already exists' });
             } else {
-                this.handleError(res, error);
+                this.handleGeneralError(res, error);
             }
         }
     }
 
-    private handleError = (res: Response, error: unknown): void => {
-        const errorMessage = (error as Error)?.message ?? 'An unexpected error occurred.';
+    public createPersona = async (req: Request, res: Response): Promise<void> => {
+        try {
+            const userId: string = req.params.userId as string;
+            const { name, text } = req.body as { name: string, text: string };
+            for (const [key, value] of Object.entries({ 'userId': userId, 'name': name, 'text': text})) {
+                if (!value) {
+                    res.status(400).json({ error: `${key} is required` });
+                    return;
+                }
+            }
+
+            if (!mongoose.Types.ObjectId.isValid(userId)) {
+                res.status(400).json({ error: 'userId is invalid' });
+                return;
+            }
+
+            const user: IUser | null = await User.findById(userId);
+            if (!user) {
+                res.status(404).json({ error: 'user with matching id not found' });
+                return;
+            }
+
+            let newPersona: IPersona = { name: name, text: text } as IPersona;
+            user.personas.push(newPersona);
+            await user.save();
+            newPersona = user.personas[user.personas.length - 1];
+            res.status(201).json({ result: newPersona });
+        } catch (error) {
+            this.handleGeneralError(res, error);
+        } 
+    }
+
+    private handleGeneralError = (res: Response, error: unknown): void => {
+        const errorMessage = (error as Error)?.message ?? 'An unexpected error occurred';
         res.status(500).json({ error: errorMessage });
     }
 }
