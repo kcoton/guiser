c867b566daec2299c5ea76ea774eef22d5fd2e8b
Brant Shapka <bshapka@gmail.com>
2024-07-18 16:40:14 -0700

    added posts field to content model

--- backend/src/models/Content.ts
+++ backend/src/models/Content.ts
@@ -1,14 +1,17 @@
 import { Schema } from 'mongoose';
 import MongooseDelete, { SoftDeleteDocument } from 'mongoose-delete';
+import { IPost, PostSchema } from './Post';
 
 export interface IContent extends SoftDeleteDocument {
     text: string;
     isRejected: boolean;
+    posts: IPost[];
 }
 
 export const ContentSchema: Schema = new Schema<IContent>({
     text: { type: String, required: true },
-    isRejected: { type: Boolean, required: true }
+    isRejected: { type: Boolean, required: true },
+    posts: [PostSchema]
 }, {
     timestamps: true
 });
