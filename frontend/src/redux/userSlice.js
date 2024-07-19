81e70177772c248cd323e5a5210667e10ac7476f
Brant Shapka <bshapka@gmail.com>
2024-07-18 21:51:24 -0700

    reorganized user slice to use db user

--- frontend/src/redux/userSlice.js
+++ frontend/src/redux/userSlice.js
@@ -1,11 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit';
-import PersonaService from '../services/PersonaService';
-import ContentService from '../services/ContentService';
 
 const userSlice = createSlice({
   name: 'user',
   initialState: {
     user: null,
+    db: null,
     sid: null
   },
   reducers: {
@@ -13,30 +12,8 @@ const userSlice = createSlice({
       state.user = action.payload.user;
       state.sid = action.payload.sid;
     },
-    init: (state) => {
-      const userId = state.user.uid;
-      const personaService = new PersonaService(userId, '668c7ce0fc7c063ca7021e5b');
-      const contentService = new ContentService(userId);
-      state.user.nextContentId = contentService.get().length + 1;
-      // TODO: replace mock call
-      const personas = personaService.getMock().map(persona => ({
-        ...persona,
-        content: contentService.get(contentEntry => contentEntry.personaId === persona.id),
-      }));
-      state.user.personas = personas;
-    },
-    addContent: (state, action) => {
-      const {personaId, text, isRejected} = action.payload;
-      const persona = state.user.personas.find(p => p.id === personaId);
-      const content = {
-        id: state.user.nextContentId++,
-        userId: state.user.uid,
-        timestamp: new Date().toISOString().replace('T', ' ').substring(0, 19),
-        text: text,
-        posted: 0,
-        isRejected: isRejected
-      };
-      persona.content.push(content);
+    storeDbUser: (state, action) => {
+      state.db = action.payload;
     },
     logout: (state) => {
       state.user = null;
@@ -45,5 +22,5 @@ const userSlice = createSlice({
   }
 });
 
-export const { sync, init, logout, addContent } = userSlice.actions;
+export const { sync, storeDbUser, logout } = userSlice.actions;
 export default userSlice.reducer;
