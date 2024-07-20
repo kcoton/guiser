33d34bbd5ecf91058e816e3884e0b9ef7dcbebb8
Brant Shapka <bshapka@gmail.com>
2024-07-20 15:26:02 -0700

    changed content service to not use hardcoded userid

--- frontend/src/services/ContentService.js
+++ frontend/src/services/ContentService.js
@@ -1,82 +1,56 @@
 import axios from 'axios';
 
-export default class ContentService {
-    constructor(userId) {
-        this.baseUrl = `${import.meta.env.VITE_BASEURL_BACK}/user`;
-        this.userId = '668c7ce0fc7c063ca7021e5b';
-        this.mockContent = [
-            {'id': 1, 'personaId': 1, 'userId': '106396242553744029996', 'timestamp': '2024-02-15 13:45:05', 'text': "Wow Costco never raised the price of the hotdog during COVID! #CostcoCares", 'posted': 0x7, 'isRejected': false},
-            {'id': 2, 'personaId': 1, 'userId': '106396242553744029996', 'timestamp': '2024-03-17 17:16:47', 'text': "I hate it when people don't return their carts and leave them wherever in the parking lot. #Animals", 'posted': 0x3, 'isRejected': false},
-            {'id': 3, 'personaId': 2, 'userId': '106396242553744029996', 'timestamp': '2023-11-11 11:11:11', 'text': "Buy my crypto course now and become a COIN CHAMP before you miss out forever!", 'posted': 0x0, 'isRejected': false},
-            {'id': 4, 'personaId': 3, 'userId': '106396242553744029996', 'timestamp': '2023-12-25 00:00:00', 'text': "Quantum gravity is like a startup that can't ship a product after 70 years! Michio Kaku is out of control!", 'posted': 0xF, 'isRejected': false}
-        ];
+const baseUrl = import.meta.env.VITE_BASEURL_BACK;
+
+async function createContent(userId, personaId, text, isRejected) {
+    if (!baseUrl) {
+        throw new Error('baseUrl is required');
     }
 
-    get(selection = (content) => true, projection = (content) => content) {
-        return this.mockContent
-            .filter(content => content.userId === this.userId && selection(content))
-            .map(content => projection(content));
-    }
-    
-    async create(personaId, text, isRejected) {
-        if (!personaId) {
-            console.error('personaId is required');
-            return;
-        }
-
-        if (!text) {
-            console.error('text is required');
-            return;
-        }
-
-        if (typeof isRejected != 'boolean') {
-            console.error('isRejected is required');
-            return;
-        }
-
-        const url = `${this.baseUrl}/${this.userId}/persona/${personaId}/content`
-        let response;
-        try {
-            response = await axios.post(url, { text, isRejected });
-        } catch (error) {
-            console.error(error);
-            return;
-        }
-
-        if (!response.data) {
-            console.error('malformed response does not contain data');
-            return;
-        }
-
-        if (!response.data.result) {
-            console.error('response does not contain result');
-            return;
-        }
-
-        return response.data.result;
+    if (!userId) {
+        throw new Error('userId is required');
     }
 
-    update(socialId) {
-        console.log('TODO: implement ContentService.update');
+    if (!personaId) {
+        throw new Error('personaId is required');
     }
 
-    delete(socialId) {
-        console.log('TODO: implement ContentService.delete');
+    if (!text) {
+        throw new Error('text is required');
     }
 
-  async generateText(persona, promptContext) {
-      const { name, text } = persona;
-      const response = await fetch(import.meta.env.VITE_BASEURL_BACK + '/content/generate/text', {
-          method: 'POST',
-          headers: {
-	        'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ personaStub: {name, text}, promptContext })
-      });
-      const data = await response.json();	 
-      if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      return data['result'];
+    if (typeof isRejected != 'boolean') {
+        throw new Error('isRejected is required');
     }
+
+    const response = await axios.post(
+        `${baseUrl}/${userId}/persona/${personaId}/content`,
+        { text, isRejected }
+    );
+
+    if (!response.data) {
+        console.error('malformed response does not contain data');
+    }
+
+    if (!response.data.result) {
+        throw new Error('response does not contain result');
+    }
+
+    return response.data.result;
+}
+
+async function generateText(persona, promptContext) {
+    const { name, text } = persona;
+    const response = await fetch(import.meta.env.VITE_BASEURL_BACK + '/content/generate/text', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ personaStub: {name, text}, promptContext })
+    });
+    const data = await response.json();	 
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return data['result'];
 }
