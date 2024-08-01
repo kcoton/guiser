56d1ed06f68e2b6e33785a2e2803e09a513d3138
Brant Shapka <bshapka@gmail.com>
2024-07-31 21:34:59 -0700

    changed app init to use redux persister

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import LandingPage from './pages/LandingPage/LandingPage';
 import LoginPage from './pages/LoginPage';
 import NavigationBar from './components/NavigationBar';
@@ -8,16 +9,18 @@ import PersonasPage from './pages/PersonasPage/PersonasPage';
 import GeneratePage from './pages/GeneratePage/GeneratePage';
 import ContentPage from './pages/ContentPage/ContentPage';
 import LogoutPage from './pages/LogoutPage';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 import './App.css';
 import ResolverPage from './pages/ResolverPage';
 
 function App() {
     return (
         <Provider store={store}>
-            <Router>
-                <MainApp />
-            </Router>
+            <PersistGate loading={null} persistor={persistor}>
+                <Router>
+                    <MainApp />
+                </Router>
+            </PersistGate>
         </Provider>
     );
 }
