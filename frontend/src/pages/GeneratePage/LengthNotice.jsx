bdc75c14790868ec847de5901cdf26e2eb420cb9
Kiara Melocoton <kiaramelocoton@gmail.com>
2024-08-05 01:11:49 -0400

    add styling for length check

--- frontend/src/pages/GeneratePage/LengthNotice.jsx
+++ frontend/src/pages/GeneratePage/LengthNotice.jsx
@@ -1,29 +1,36 @@
-import Box from '@mui/material/Box';
+/* eslint-disable react/prop-types */
+import { Box, Typography, Stack } from '@mui/material';
 import CheckIcon from '@mui/icons-material/Check';
 import CloseIcon from '@mui/icons-material/Close';
 
 export default function LengthNotice({ socialApps, contentLength }) {
     return (
         contentLength > 0 && (
-            <Box className='generate-page-pane'>
-                <span>Verify that the length of your content meets platform requirements</span>
-                <ul>
-                    <li>Content Length: {contentLength}</li>
-                    {socialApps.map((app) => {
-                        const icon =
-                            contentLength <= app.maxTextLength ? (
-                                <CheckIcon style={{ color: 'green' }} />
-                            ) : (
-                                <CloseIcon style={{ color: 'red' }} />
+        <Box sx={{ mx: 11, mt: 1, boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.1)' }}>
+            <Stack direction='row' spacing={2}>
+                <Typography variant='caption' color='secondary'>
+                    Verify that the length of your content meets platform requirements:
+                </Typography>
+                <Typography variant='caption'>
+                    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                        Content Length: {contentLength} {'->'}
+                        {socialApps.map((app) => {
+                            const icon =
+                                contentLength <= app.maxTextLength ? (
+                                    <CheckIcon color='success' sx={{ fontSize: 16 }} />
+                                ) : (
+                                    <CloseIcon color='error' sx={{ fontSize: 16 }} />
+                                );
+                            return (
+                                <Box key={app.seqNo} sx={{ display: 'flex', alignItems: 'center', ml: 1 }}>
+                                    {app.name}: {app.maxTextLength}{icon}
+                                </Box>
                             );
-                        return (
-                            <li key={app.seqNo}>
-                                {app.name}: {app.maxTextLength} {icon}
-                            </li>
-                        );
-                    })}
-                </ul>
-            </Box>
+                        })}
+                    </Box>
+                </Typography>
+            </Stack>
+        </Box>
         )
     );
 }
