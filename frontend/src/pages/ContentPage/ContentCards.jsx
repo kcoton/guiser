136b46c34403da3c96c2b4be67d6a361f493455a
Brant Shapka <bshapka@gmail.com>
2024-08-04 23:25:09 -0700

    added token checking to cards

--- frontend/src/pages/ContentPage/ContentCards.jsx
+++ frontend/src/pages/ContentPage/ContentCards.jsx
@@ -8,9 +8,13 @@ import { Grid } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 import { updatePosted } from '../../redux/userSlice';
 import { postToApp } from '../../services/ContentService';
+import { isTokenValid } from '../PersonasPage/Common';
 
 export default function ContentCards({ socialApps, selectedContent, setSelectedContent }) {
     const userId = useSelector((state) => state.user.db?._id);
+    const tokens = useSelector((state) => 
+        state.user.db?.personas?.find(p => p._id === selectedContent.personaId)?.authTokens
+    ) ?? [];
     const dispatch = useDispatch();
 
     async function handlePostButtonClick(userId, personaId, contentId, appSeqNo) {
@@ -28,24 +32,38 @@ export default function ContentCards({ socialApps, selectedContent, setSelectedC
         dispatch(updatePosted({ personaId, contentId, posted: response }));
     }
 
+    function cardAlertMessage(contentTooLong, token, tokenIsExpired) {
+        if (contentTooLong) {
+            return 'Content too long for app';
+        }
+        if (!token) {
+            return 'Not connected to app. Connect using Personas page.';
+        }
+        if (tokenIsExpired) {
+            return 'Token is expired. Reconnect using Personas page.';
+        }
+    }
+
     return (
         <Grid container spacing={4} style={{ marginTop: 10 }}>
             {socialApps.map((app) => {
                 const isPostedToSite = selectedContent.posted & (2 ** (app.seqNo - 1));
                 const contentTooLong = selectedContent.text.normalize("NFC").length > app.maxTextLength;
+                const token = tokens.find(t => t.platform === app.name);
+                const tokenIsExpired = token && token.expiry && !isTokenValid(token.expiry);
                 return (
                     <Grid item xs={12} sm={6} md={4} key={app.seqNo}>
                         <Card sx={{ minWidth: 20 }}>
                             <CardContent>
                                 <Typography sx={{ fontSize: 14 }} color='text.secondary' gutterBottom>
-                                    {isPostedToSite ? '' : 'Not'} Posted {contentTooLong ? '- Too Long for App' : ''}
+                                    {isPostedToSite ? 'Posted' : 'Not Posted'}
                                 </Typography>
                                 <Typography variant='h5' component='div'>
                                     {app.name}
                                 </Typography>
                             </CardContent>
                             <CardActions>
-                                {!isPostedToSite && !contentTooLong ? (
+                                {!isPostedToSite && !contentTooLong && token && !tokenIsExpired ? (
                                     <Button
                                         size='small'
                                         onClick={() =>
@@ -60,7 +78,9 @@ export default function ContentCards({ socialApps, selectedContent, setSelectedC
                                         Post
                                     </Button>
                                 ) : (
-                                    ''
+                                    <Typography sx={{ fontSize: 12 }} color='text.secondary' gutterBottom>
+                                        {isPostedToSite ? '' : cardAlertMessage(contentTooLong, token, tokenIsExpired)}
+                                    </Typography>
                                 )}
                             </CardActions>
                         </Card>
