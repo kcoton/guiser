bccb5c8992e99d425bd9656690a342cb7c50a58e
Kiara Melocoton <kiaramelocoton@gmail.com>
2024-08-06 01:19:32 -0400

    add conditional text for generate

--- frontend/src/pages/GeneratePage/PersonaCardCarousel.jsx
+++ frontend/src/pages/GeneratePage/PersonaCardCarousel.jsx
@@ -1,15 +1,26 @@
 /* eslint-disable react/prop-types */
 import Slider from 'react-slick';
-import { Box } from '@mui/material';
+import { Box, useMediaQuery } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import { sliderSettings } from '../Common';
 import { PersonaCard } from '../../components/PersonaComponent';
+import { Fragment } from 'react';
 
 export default function PersonaCardCarousel({ personas, selectedPersona, onSelectPersonaClick }) {    
+    const isMediumUp = useMediaQuery((theme) => theme.breakpoints.up('md'));
+
     return (
         <>
             <Typography variant='overline' noWrap component='div' sx={{ letterSpacing: 2, fontSize: { xs: 10.5, sm: 16, md: 20, lg: 24 }, mx: { xs: 5, md: 5, lg: 10 }, mb: 1 }}>
-              <span style={{ color: '#A688FA' }}>Generate</span> Content for which persona?
+            {isMediumUp ? (
+                <Fragment>
+                    <span style={{ color: '#A688FA' }}>Generate</span> content for which persona?
+                </Fragment>
+                ) : 
+                <Fragment>
+                    <span style={{ color: '#A688FA' }}>Generate</span> content for who?
+                </Fragment>
+            }   
             </Typography>
             <Box sx={{ mx: 8, mb: 4 }}>
                 {personas?.length ? (
