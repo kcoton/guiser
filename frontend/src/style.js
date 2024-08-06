1cbb5288460a019caaccda156814e8dc131bc8ae
Kiara Melocoton <kiaramelocoton@gmail.com>
2024-08-05 23:07:50 -0400

    add breakpoints custom

--- frontend/src/style.js
+++ frontend/src/style.js
@@ -1,4 +1,14 @@
+
 export const themeOptions = {
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 768, // change md to 768
+      lg: 1200,
+      xl: 1536,
+    },
+  },
   palette: {
     type: 'dark',
     primary: {
